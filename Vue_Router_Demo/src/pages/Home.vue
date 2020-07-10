4f79fb9c41a479ad5b65b318b95af93d3285db20
bibi2808 <tuanda28081995@gmail.com>
2020-07-10 10:21:12 +0900

    vue-router demo

---
+++ Vue_Router_Demo/src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <b-container>
+        <b-jumbotron header="HomePage">
+            <p>For more information visit website</p>
+            <b-button variant="primary" href="#">More Info</b-button>
+        </b-jumbotron>
+    </b-container>
+</template>
+
+<script>
+export default {
+    name: "home"
+};
+</script>
+
+<style>
+</style>
---
+++ Vue_Router_Demo/src/pages/HomePage.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <transition name="slide-fade" mode="out-in">
+            <router-view></router-view>
+        </transition>
+        <slider />
+    </div>
+</template>
+
+<script>
+import Slider from "../components/Slider";
+export default {
+    name: "home-page",
+    components: {
+        Slider
+    }
+};
+</script>
+
+<style>
+.slide-fade-enter-active {
+    transition: all 0.3s ease;
+}
+.slide-fade-leave-active {
+    transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* Trước 2.1.8 thì dùng .slide-fade-leave-active */ {
+    transform: translateX(10px);
+    opacity: 0;
+}
+</style>
