ecfac501cd329524c52d680a69da6b07e5a48ab0
bibi2808 <tuanda28081995@gmail.com>
2020-07-13 11:08:20 +0900

    修正

--- 05-blog_Vue/src/pages/NotFoundPage.vue
+++ 05-blog_Vue/src/pages/NotFoundPage.vue
@@ -1,9 +1,26 @@
 <template>
     <div class="page-404">
-        <div>
-            <img src="https://doyouconvert.com/wp-content/uploads/2018/04/404_Error.jpg" alt="404" />
-        </div>
-        <router-link tag="button" to="/" type="submit" class="btn btn-primary">Go to HomePage</router-link>
+        <section class="page_404">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="col-sm-10 col-sm-offset-1 text-center">
+                            <div class="four_zero_four_bg">
+                                <h1 class="text-center">404</h1>
+                            </div>
+
+                            <div class="contant_box_404">
+                                <h3 class="h2">Look like you're lost</h3>
+
+                                <p>the page you are looking for not avaible!</p>
+
+                                <router-link to="/" type="submit" class="link_404">Go to HomePage</router-link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 </template>
 
@@ -14,7 +31,38 @@ export default {
 </script>
 
 <style>
-.page-404 {
-    text-align: center;
+.page_404 {
+    padding: 40px 0;
+    background: #fff;
+    font-family: "Arvo", serif;
+}
+
+.page_404 img {
+    width: 100%;
+}
+
+.four_zero_four_bg {
+    background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+    height: 400px;
+    background-position: center;
+}
+
+.four_zero_four_bg h1 {
+    font-size: 80px;
+}
+
+.four_zero_four_bg h3 {
+    font-size: 80px;
+}
+
+.link_404 {
+    color: #fff !important;
+    padding: 10px 20px;
+    background: #39ac31;
+    margin: 20px 0;
+    display: inline-block;
+}
+.contant_box_404 {
+    margin-top: -50px;
 }
 </style>
